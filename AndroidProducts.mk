9a35eda9b9e6fa3a83e3435f63944b5eddfcfa07
Adam White <ruinwmd666@gmail.com>
2020-03-31 20:59:48 +1100

    Magma Bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_cepheus.mk
+    $(LOCAL_DIR)/magma_cepheus.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_cepheus-userdebug \
-    derp_cepheus-eng
+    magma_cepheus-userdebug \
+    magma_cepheus-eng
